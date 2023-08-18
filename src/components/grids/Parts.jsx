cca82f55cc727b0c4bdfd9fc6b013b719660f05a
Gizem Şangür <gizem2002sangur@gmail.com>
2023-08-18 21:54:03 +0300

    Make background images and add headings

--- src/components/grids/Parts.jsx
+++ src/components/grids/Parts.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import styles from "./page.module.css";
-import { Grid } from "@mui/material";
+import { BsArrowRight } from "react-icons/bs";
+import { Grid, Typography } from "@mui/material";
 const Parts = (props) => {
   return (
     <Grid container className={styles.container}>
@@ -8,12 +9,20 @@ const Parts = (props) => {
         <Grid item xs={6}>
           <Grid container className={styles.insidecontainer}>
             <Grid item xs={6} className={styles.item}>
-              <Grid className={styles.photo}></Grid>
+              <Grid className={styles.photo1}>
+                <Grid sx={{ justifyContent: "center", alignItems: "center" }}>
+                  <Typography className={styles.heading}>50+</Typography>
+                  <Typography className={styles.heading}>Partner</Typography>
+                  <button className={styles.button}>Discover More</button>
+                </Grid>
+              </Grid>
             </Grid>
             <Grid item xs={6}>
               <Grid container direction="column" className={styles.row}>
                 <Grid item xs={12} className={styles.itemd}>
-                  <Grid className={styles.photo}></Grid>
+                  <Grid className={styles.photo}>
+                    <BsArrowRight color="white" size="64px" />
+                  </Grid>
                 </Grid>
               </Grid>
               <Grid className={styles.ball}></Grid>
@@ -22,6 +31,11 @@ const Parts = (props) => {
         </Grid>
         <Grid item xs={6}>
           <Grid className={styles.ball}></Grid>
+          <Grid sx={{padding:"12px",margin:"12px"}}> 
+            <Typography className={styles.heading}>What we do</Typography>
+            <Typography sx={{fontSize:"24px",color:"white"}}>8 Service</Typography>
+            <Grid sx={{width:"50%",border:"1px solid white"}}></Grid>
+          </Grid>
         </Grid>
       </Grid>
       <Grid container direction="row" className={styles.row}>
